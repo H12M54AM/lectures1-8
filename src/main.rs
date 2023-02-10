d26b19fe9afd5fd4016b57965b9001fff31e230a
Edward Naidoo <securedbread@protonmail.com>
2023-02-10 12:59:09 -0800

    Made more outputs to see what the results will be

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use rand::prelude::*;
 
 fn main() {
-  // let x = gen_num::Num{max: 10}
-  let num = rand::thread_rng().gen::<i32>();
-  // stats::mean();
-  println!("Random Number: {}", num);
+  let max = 100;
+  let num_i = rand::thread_rng().gen::<i32>();
+  let num_f = rand::thread_rng().gen::<f32>();
+  let num = rand::thread_rng().gen_range(0..max);
+
+  println!("Max Number (Int)  : {}", num_i);
+  println!("Max Number (Float): {}", num_f);
+  println!("Random Number     : {}", num);
 }
