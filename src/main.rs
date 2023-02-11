dbb7348aca813239a280f6eec1e11335a3402a50
Edward Naidoo <securedbread@protonmail.com>
2023-02-11 00:11:13 -0800

    Fixed the code and added comments for better documentation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+/**
+ * Program Displays random variables from the 
+ * rand crate and utilizes two functions to 
+ * compare the long and short version of the 
+ * variable.
+ * 
+ * Edward Naidoo
+ * Feb 8, 2023
+ * BCIT - Business Stats 
+ */
+
 use rand::prelude::*;
 
 fn main() {
@@ -7,17 +18,30 @@ fn main() {
   let num = rand::thread_rng().gen_range(0..max);
 
   short(num_i, num_f, num);
+  println!("");
   long(num_i, num_f, num);
 }
 
+/**
+ * Short Version
+ * Outputs the values as a 32 Bit int and float
+ * from randomized variable.
+ */
 fn short(num_i:i32, num_f:f32, num:i32) {
-  println!("Max Number (Int)  : {}", num_i);
-  println!("Max Number (Float): {}", num_f);
-  println!("Random Number     : {}", num);
+  println!("Following as a 32 bit value");
+  println!("Max Number (Int)   : {}", num_i);
+  println!("Max Number (Float) : {}", num_f);
+  println!("Random Number      : {}", num);
 }
 
-fn long(num_i:i64, num_f:f64, num:i64) {
-  println!("Max Number (Int)  : {}", num_i);
-  println!("Max Number (Float): {}", num_f);
-  println!("Random Number     : {}", num);
+/**
+ * Long Version
+ * Outputs the values as a 64 Bit int and float
+ * from randomized variable. 
+ */
+fn long(num_i:i32, num_f:f32, num:i32) {
+  println!("Following as a 64 bit value");
+  println!("Max Number (Int)   : {}", num_i as i64);
+  println!("Max Number (Float) : {}", num_f as f64);
+  println!("Random Number      : {}", num as i64);
 }
